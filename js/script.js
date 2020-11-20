c37c36266744abbd309c33b6008511797da6073d
javier <surtich@gmail.com>
2020-11-20 03:09:05 +0100

    Added generic menu-item templates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-function printMenu() {
+function numberMenu(menu, prefix="") {
+    let i = 0;
+    return menu.map(function(item) {
+        if (Array.isArray(item)) {
+            return numberMenu(item, `${i}.`)
+        } else {
+            i++;
+            return `${prefix}${i}. ${item}`;
+        }
+    });
+}
+
+function createMenuItems() {
+    numeredMenu = numberMenu(menu);
+
+    const menuItemTemplate = document.getElementById("menu-item");
+    const menuList = document.querySelector(".navigation__list");
+    var fragment = new DocumentFragment();
+
+    for (let item of numeredMenu) {
+        const menuItem = menuItemTemplate.content.cloneNode(true);
+        fragment.appendChild(menuItem);
+    }
+
+    menuList.appendChild(fragment);
+
+
+    
 
 }
 
-printMenu();
+createMenuItems();
